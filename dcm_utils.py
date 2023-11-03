608aa56432f08341470bac69069ededeefaa78dc
ren-joey <tsuyiren@gmail.com>
2023-11-03 09:17:12 +0800

    feat: 🎸 add copy empty directory tree func

--- dcm_utils.py
+++ dcm_utils.py
@@ -1,9 +1,9 @@
 import os
 import pickle
 import re
-
+import shutil
 import pydicom
-
+import cv2
 from plotter import img_plotter
 
 
@@ -49,11 +49,50 @@ def dcm_tag_ck(dcm=None, tagname=None):
 
     return False, tagname, tag_pos_list[len(tag_pos_list) - 1]
 
+def cptree(src, dst):
+
+    def keep_only_dirs(path, files):
+        to_ignore = [
+            fname for fname in files
+            if not os.path.isdir(os.path.join(path, fname))
+            ]
+        return to_ignore
+
+    # This works for python3 (<3.8), BUT the target directory MUST not exist
+    # shutil.copytree(src, dst, ignore=keep_only_dirs)
+
+    # For python 3.8 you can use following code (Here the target directory can
+    # already exist and only new directories will be added
+    shutil.copytree(src, dst, ignore=keep_only_dirs, dirs_exist_ok=True)
+
+def dcm2png(dcm_dir):
+    for dir_idx, (dir_path, dir_names, file_names) in enumerate(os.walk(dcm_dir)):
+
+        regex = '^(?!\.).*\.dcm$'
+
+        for file_idx, file in enumerate(file_names):
+            is_dcm = re.fullmatch(regex, file) is not None
+
+            if is_dcm:
+                file_path = os.path.join(dir_path, file)
+                ds = pydicom.read_file(file_path)
+                img = ds.pixel_array # get image array
+                cv2.imwrite(os.path.join(dir_path, file.replace('.dcm', '.png')), img)
+
 if __name__ == '__main__':
+    ### dcm file plot
     # dcm_dir = '/Volumes/Transcend/RTMets_datasets/P214900000003/AC0003002/FO-5677511027863284789.dcm'
     # dcm = pydicom.dcmread(dcm_dir, force=True)
     # res, tagname, _ = dcm_tag_ck(dcm)
     # img_plotter(dcm)
-    res, tagname, _ = dcm_tag_ck(tagname='Localizers')
-    print(res)
-    print(tagname)
+
+    ### tag check test
+    # res, tagname, _ = dcm_tag_ck(tagname='Localizers')
+    # print(res)
+    # print(tagname)
+
+    ### cptree test
+    # cptree('E:\\RTMets_datasets', 'C:\\Users\\axe09\\Desktop\\L3')
+
+    ### dcm2png test
+    dcm2png('C:\\Users\\axe09\\Desktop\\L3')
