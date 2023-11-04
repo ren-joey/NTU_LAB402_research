ff77e3993e6d73988bd315f83aa752ce08f4b540
ren-joey <tsuyiren@gmail.com>
2023-11-04 08:50:35 +0800

    fix: 🐛 fix some TODO: issues

--- dcm_run_and_del.py
+++ dcm_run_and_del.py
@@ -3,41 +3,59 @@ import re
 import requests
 from tqdm import tqdm
 
+dcm2niix_path = 'C:\\Users\\axe09\\Desktop\\dcm2niix.exe'
+sarcopenia_server_url = 'http://localhost:5000/predict'
 
-def dcm_run_and_del(in_dir, out_dir):
+def dcm_run_and_del(
+    in_dir,
+    out_dir,
+    dcm2niix_path,
+    sarcopenia_server_url
+):
     in_dir = os.path.join(in_dir)
     out_dir = os.path.join(out_dir)
 
     os.makedirs(out_dir, exist_ok=True)
 
+    # some function's constant
+    direction_name_list = ['seg', 'frontal']
+    patient_regex = '[\\\/]P\d{12}[\\\/]AC\d{7}'
+    nii_ext_regex = '^(?!\.).*\.nii$'
+
     for dir_idx, (dir_path, dir_names, file_names) in enumerate(os.walk(in_dir)):
+
+        # TODO: circuit breaker
         if dir_idx > 5:
             break
 
-        regex = '[\\\/]P\d{12}[\\\/]AC\d{7}'
-        res = re.search(regex, dir_path)
+        res = re.search(patient_regex, dir_path)
 
+        # make sure that directory is a patient folder (named AC\d{7})
         if res is not None:
+            patient = res.group()
             niix_out_dir = os.path.normpath(out_dir + res.group() + '_niix')
 
-            # delete_dir(niix_out_dir)
+            # convert and compress dcm files into niix file
             os.makedirs(niix_out_dir, exist_ok=True)
-            # os.system(f'dcm2niix -o {niix_out_dir} {dir_path}')
-            command = f'C:\\Users\\axe09\\Desktop\\dcm2niix.exe -o {niix_out_dir} {dir_path}'
+            command = f'{dcm2niix_path} -o {niix_out_dir} {dir_path}'
             os.system(command)
 
+            # list the proceeded niix files
+            # and upload them to the sarcopenia-ai server individually
             for nii_file_names in os.list(niix_out_dir):
-                regex = '^(?!\.).*\.nii$'
-                is_nii = re.fullmatch(regex, nii_file_names) is not None
 
-                ## TODO::
-                ## 以下還沒改完
+                # identify niix files by file extension
+                is_nii = re.fullmatch(nii_ext_regex, nii_file_names) is not None
+
+                ## TODO:
                 ## sarcopenia-ai 那邊還要改成紫色飽滿
+
+                # if the file extension matched
                 if is_nii:
                     form = {
                         'image': (nii_file_names, open(os.path.join(dir_path, nii_file_names), 'rb'))
                     }
-                    res = requests.post(url, files=form)
+                    res = requests.post(sarcopenia_server_url, files=form)
 
                     if res.status_code == 200:
                         res = res.json()
@@ -45,15 +63,21 @@ def dcm_run_and_del(in_dir, out_dir):
                         z = res['prediction']['slice_z'] if 'slice_z' in res['prediction'] else 'inf'
 
                         for direction_name in direction_name_list:
-                            seg = requests.get(f'http://localhost:5000/uploads/{nii_file_names}_{direction_name}-{id}.jpg')
+                            for i in range(1, 3):
+                                postfix = i if i != 1 else ''
+                                seg = requests.get(f'http://localhost:5000/uploads/{nii_file_names}_{direction_name}-{id}{postfix}.jpg')
 
-                            if seg.status_code == 200:
-                                out_fullpath = os.path.join(out_dir, patient)
-                                os.makedirs(out_fullpath, exist_ok=True)
-                                img = open(f'{out_fullpath}/{nii_file_names}_{direction_name}_slicez-{z}.jpg', 'wb')
-                                img.write(seg.content)
-                                img.close()
+                                if seg.status_code == 200:
+                                    out_fullpath = os.path.join(out_dir, patient)
+                                    os.makedirs(out_fullpath, exist_ok=True)
+                                    img = open(f'{out_fullpath}/{nii_file_names}_{direction_name}_slicez-{z}.jpg', 'wb')
+                                    img.write(seg.content)
+                                    img.close()
 
 
 if __name__ == '__main__':
-    dcm_run_and_del('E:\\RTMets_datasets\\P214900000002', 'E:\\temp')
+    dcm_run_and_del(
+        'E:\\RTMets_datasets\\P214900000002',
+        'E:\\temp',
+        dcm2niix_path, sarcopenia_server_url
+    )
