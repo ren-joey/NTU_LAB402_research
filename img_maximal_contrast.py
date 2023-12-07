db3ab67bd92862aa4065ed5dad039378cc2d6b31
ren-joey <tsuyiren@gmail.com>
2023-12-07 15:53:42 +0800

    feat: 🎸 add img_anomaly_correct func

--- img_maximal_contrast.py
+++ img_maximal_contrast.py
@@ -4,6 +4,29 @@ from os.path import splitext, isfile, join
 from os import listdir
 from pathlib import Path
 
+def img_anomaly_correct(file):
+    file = Path(file)
+    mask = np.asarray(Image.open(file))
+    mask = mask.copy()
+    modified = False
+    for y, row in enumerate(mask):
+        for x, pixel in enumerate(row):
+            _ = pixel.tolist()
+            if _ == [255, 0, 255]:
+                modified = True
+                mask[y][x] = np.array([255, 0, 0])
+            elif _ == [255, 255, 0]:
+                modified = True
+                mask[y][x] = np.array([255, 0, 0])
+            elif _ == [0, 255, 255]:
+                modified = True
+                mask[y][x] = np.array([0, 0, 255])
+            elif _ == [255, 255, 255]:
+                modified = True
+                mask[y][x] = np.array([255, 0, 0])
+
+    if modified == True:
+        Image.fromarray(mask).save(Path(file.parent, f'{file.stem}.png'))
 
 
 def img_maximal_contrast(file):
@@ -19,17 +42,15 @@ def img_maximal_contrast(file):
                     mask[y][x][z] = 255
                 elif value <= 127:
                     mask[y][x][z] = 0
-    print(file)
+
     Image.fromarray(mask).save(Path(file.parent, f'{file.stem}.png'))
 
-    # print(mask)
-    # print(mask.ndim)
-    # mask = mask.reshape(-1, mask.shape[-1])
-    # print(np.unique(mask, axis=0))
 
 
 if __name__ == '__main__':
-    path = "G:\\datasets\\muscle_group_segment\\ydata"
+    path = "/Users/joey_ren/Desktop/muscle_group_segment/ydata"
     files = [file for file in listdir(path) if isfile(join(path, file)) and not file.startswith('.') and file.endswith(".png")]
     for idx, file in enumerate(files):
-        unique_mask_values(join(path, file))
+        file_path = join(path, file)
+        img_maximal_contrast(file_path)
+        img_anomaly_correct(file_path)
