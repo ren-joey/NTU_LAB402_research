736521bd8487bb0b61a9c2bfcf0b44a9cbc6fdae
ren-joey <tsuyiren@gmail.com>
2023-12-26 02:38:15 +0800

    feat: 🎸 add greyscale maximal contrast method

--- img_maximal_contrast.py
+++ img_maximal_contrast.py
@@ -45,12 +45,43 @@ def img_maximal_contrast(file):
 
     Image.fromarray(mask).save(Path(file.parent, f'{file.stem}.png'))
 
+def img_maximal_contrast_grey(file, threshold=127):
+    file = Path(file)
+    mask = np.asarray(Image.open(file).convert('L'))
+    mask = mask.copy()
+    for y, row in enumerate(mask):
+        for x, pixel in enumerate(row):
+            try:
+                if pixel == 0 or pixel == 255:
+                    continue
+                elif pixel > threshold:
+                    mask[y][x] = 255
+                elif pixel <= threshold:
+                    mask[y][x] = 0
+            
+            # when the image does not contain 1 channel
+            except:
+                isNpArr = isinstance(pixel, np.ndarray);
+                print('true' if isNpArr else 'false')
+                print(file)
+                raise
+
+    Image.fromarray(mask).save(Path(file.parent, f'{file.stem}.png'))
 
 
 if __name__ == '__main__':
-    path = "/Users/joey_ren/Desktop/muscle_group_segment/ydata"
-    files = [file for file in listdir(path) if isfile(join(path, file)) and not file.startswith('.') and file.endswith(".png")]
-    for idx, file in enumerate(files):
-        file_path = join(path, file)
-        img_maximal_contrast(file_path)
-        img_anomaly_correct(file_path)
+    paths = [
+        "C:\\Users\\axe09\\Desktop\\NTU_LAB402_research_code\\datasets\\muscle_segment\\train\\ydata",
+        "C:\\Users\\axe09\\Desktop\\NTU_LAB402_research_code\\datasets\\muscle_segment\\validation\\ydata"
+    ]
+    for path in paths:
+        files = [file for file in listdir(path) if isfile(join(path, file)) and not file.startswith('.') and file.endswith(".png")]
+        for idx, file in enumerate(files):
+            file_path = join(path, file)
+
+            # for 3 channels image
+            # img_maximal_contrast(file_path)
+            # img_anomaly_correct(file_path)
+
+            # for 1 channel image
+            img_maximal_contrast_grey(file_path);
